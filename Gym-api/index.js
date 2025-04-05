e66dd6ae6b60318df1d79a33d16e20695a501271
palakRana11 <palakranag99@gmail.com>
2025-04-05 23:25:28 +0530

    Iteration 2 for GymGuru project

--- Gym-api/index.js
+++ Gym-api/index.js
@@ -123,6 +123,26 @@ app.get('/food/search/:name', verifyToken, async (req, res) => {
     }
 });
 
+
+//  endpoint to get user details
+app.get('/username', verifyToken, async (req, res) => {
+    try {
+      const user = await userModel.findById(req.user.id).select('name age');
+      
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.status(200).json({ name: user.name, age: user.age });
+  
+    } catch (err) {
+      console.error("Username Fetch Error:", err);
+      res.status(500).json({ message: "Error fetching username" });
+    }
+  });
+  
+
+
 //middleware to verify JWT token
 
 function verifyToken(req, res, next) {
